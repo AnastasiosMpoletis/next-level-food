c0dcbac89b7dbae9ffbbdde7de529a46267cc547
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-06-29 11:59:24 +0300

    L-436. Next Level Food app. Initial commit from course

--- app/page.js
+++ app/page.js
@@ -1,15 +1,9 @@
-import Link from "next/link";
-/**
- * @ points to the root folder @see ./jsconfig.json.
- */
-import Header from "@/components/header.js";
-
 export default function Home() {
   return (
     <main>
-      <Header />
-      <p>🔥 Let&apos;s get started! 🔥</p>
-      <p><Link href="/about">About us</Link></p>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
