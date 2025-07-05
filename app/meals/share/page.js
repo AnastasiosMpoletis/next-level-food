64f1dd60a03a4320d04083e9dc6a0f58ba99acd9
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-07-05 13:42:50 +0300

    L-465. Create MealsFormSubmit component which renders the submit button that changes based on the submit status

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,7 @@
 import ImagePicker from '@/components/meals/image-picker.js';
 import { shareMeal } from '@/lib/actions.js';
 import classes from './page.module.css';
+import MealsFormSubmit from '@/components/meals/meals-form-submit.js';
 
 export default function ShareMealPage() {
   return (
@@ -42,7 +43,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
